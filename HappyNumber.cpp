5bd2201b713b81349dda658e8b5fe0eef9f32e9f
Hala <47666430+Hala-H@users.noreply.github.com>
2020-10-20 12:39:53 +0300

    Add files via upload

---
+++ HappyNumber.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int count = 0;
+        int temp = n;
+        int sum = 0;
+
+        unordered_set<int> seen;
+        while(n != 1)
+        {
+            sum = 0;
+            while(temp != 0)
+            {
+                temp /= 10;
+                count++;
+            }
+            int digits[count];
+            for(int i = 0; i < count; i++)
+            {
+                digits[i] = n % 10; 
+                n /= 10;
+            }
+            for(int j = 0; j < count; j++)
+            {
+                sum += digits[j]*digits[j]; 
+            }
+            if(seen.count(sum) > 0)
+                return false;
+            else
+                seen.insert(sum);
+            
+            temp = sum;
+            n = sum;
+        }
+        return true;   
+    }
+};
